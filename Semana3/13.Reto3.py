f45b5f785f56235835f0a6f189226733bcac5daf
Calletana López Baleta <72431798+KytanaLopez@users.noreply.github.com>
2022-06-03 18:19:56 -0500

    Update 13.Reto3.py

--- Semana3/13.Reto3.py
+++ Semana3/13.Reto3.py
@@ -34,28 +34,3 @@ print(AutoPartes([
     (2010,'bujía', 'ER6523',9,36,'Pedro Montes',1243,'12/06/2020'),
     (3578,'tijera', 'QW8523',1,128,'Pedro Faria',1456,'12/06/2020'),
     (9251,'piñón', 'EN5698',2,8,'Juan Peña',565,'12/06/2020')]), 2010)
-
-
-
-#Copiado de plataforma
-"""def AutoPartes(ventas:list):
-    dic = {}
-    for i in range(len(ventas)):
-        dic[i] = [ventas[i]]
-    return dic
-   
-def consultaRegistro(ventas,idProducto):
-    dic = {}
-    respuesta=''
-    for i in range(len(ventas)) :
-        if ventas[i][0][0] == idProducto:
-            dic[i] = ventas[i]
-          
-    if len(dic) == 0:
-        respuesta = 'No hay registro de venta de ese producto'
-    
-    for valoresDic in dic.values(): 
-        for idProducto,dProducto,pnProducto,cvProducto,sProducto,nComprador,cComprador,fVenta in valoresDic:
-            respuesta += (f'Producto consultado : {idProducto}  Descripción  {dProducto}  #Parte  {pnProducto}  Cantidad vendida  {cvProducto}  Stock  {sProducto}  Comprador {nComprador}  Documento  {cComprador}  Fecha Venta  {fVenta}\n')      
-    print(respuesta)"""
-
